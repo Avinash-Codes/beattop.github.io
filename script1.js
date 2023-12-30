1df87658955fc650ac65e7652e7eb2a3135fa3f9
Avinash <donotsmile9@gmail.com>
2023-12-31 00:19:22 +0530

    added new things/ in web

--- script1.js
+++ script1.js
@@ -1,16 +1,12 @@
 const API_KEY = "api_key=35f5e77c6f4deaf531ee2c27fff40aea";
 const BASE_URL = "https://api.themoviedb.org/3";
-const SEARCH_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const SEARCH_URL = BASE_URL + '/discover/tv?sort_by=popularity.desc&'+API_KEY;
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
 
 const genres = [
     {
-       "id":28,
-       "name":"Action"
-    },
-    {
-       "id":12,
-       "name":"Adventure"
+       "id":10759,
+       "name":"Action & Adventure"
     },
     {
        "id":16,
@@ -37,44 +33,36 @@ const genres = [
        "name":"Family"
     },
     {
-       "id":14,
-       "name":"Fantasy"
-    },
-    {
-       "id":36,
-       "name":"History"
-    },
-    {
-       "id":27,
-       "name":"Horror"
-    },
-    {
-       "id":10402,
-       "name":"Music"
+       "id":10762,
+       "name":"Kids"
     },
     {
        "id":9648,
        "name":"Mystery"
     },
     {
-       "id":10749,
-       "name":"Romance"
+       "id":10763,
+       "name":"News"
     },
     {
-       "id":878,
-       "name":"Science Fiction"
+       "id":10764,
+       "name":"Reality"
     },
     {
-       "id":10770,
-       "name":"TV Movie"
+       "id":10765,
+       "name":"Sci-Fi & Fantasy"
     },
     {
-       "id":53,
-       "name":"Thriller"
+       "id":10766,
+       "name":"Soap"
     },
     {
-       "id":10752,
-       "name":"War"
+       "id":10767,
+       "name":"Talk"
+    },
+    {
+       "id":10768,
+       "name":"War & Politics"
     },
     {
        "id":37,
